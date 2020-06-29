136aea7a88a66c2b449c51744f303f9cb1c86ab3
Mantager <joecann09@gmail.com>
2020-06-29 19:53:34 +0100

    Episode #1

--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -586,6 +586,7 @@
 
 // Map Group 33
 #define MAP_ROUTE124_DIVING_TREASURE_HUNTERS_HOUSE (0 | (33 << 8))
+#define MAP_MANTA_ZONE                             (1 | (33 << 8))
 
 #define MAP_GROUPS_COUNT 34
 
